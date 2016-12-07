10594961a029a359575e41a3c869af684e2318b2
tc021 <sypnik21@o2.pl>
2016-12-07 17:01:05 +0100

    Funkcja DodajUsuń Studenta dla admina

---
+++ Proj/BazaUniwersytecka/AddDelStu.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace BazaUniwersytecka
+{
+    partial class AddDelStu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nrAlbBox = new System.Windows.Forms.TextBox();
+            this.ImBox = new System.Windows.Forms.TextBox();
+            this.NazBox = new System.Windows.Forms.TextBox();
+            this.WydzBox = new System.Windows.Forms.TextBox();
+            this.KierBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.nrAlbumuBox = new System.Windows.Forms.TextBox();
+            this.DelStud = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
+            this.Quit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dodaj Studenta";
+            // 
+            // nrAlbBox
+            // 
+            this.nrAlbBox.Location = new System.Drawing.Point(30, 84);
+            this.nrAlbBox.Name = "nrAlbBox";
+            this.nrAlbBox.Size = new System.Drawing.Size(100, 20);
+            this.nrAlbBox.TabIndex = 1;
+            // 
+            // ImBox
+            // 
+            this.ImBox.Location = new System.Drawing.Point(136, 84);
+            this.ImBox.Name = "ImBox";
+            this.ImBox.Size = new System.Drawing.Size(100, 20);
+            this.ImBox.TabIndex = 1;
+            // 
+            // NazBox
+            // 
+            this.NazBox.Location = new System.Drawing.Point(242, 84);
+            this.NazBox.Name = "NazBox";
+            this.NazBox.Size = new System.Drawing.Size(126, 20);
+            this.NazBox.TabIndex = 1;
+            // 
+            // WydzBox
+            // 
+            this.WydzBox.Location = new System.Drawing.Point(374, 84);
+            this.WydzBox.Name = "WydzBox";
+            this.WydzBox.Size = new System.Drawing.Size(121, 20);
+            this.WydzBox.TabIndex = 1;
+            // 
+            // KierBox
+            // 
+            this.KierBox.Location = new System.Drawing.Point(501, 84);
+            this.KierBox.Name = "KierBox";
+            this.KierBox.Size = new System.Drawing.Size(122, 20);
+            this.KierBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Podaj Nr Albumu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(133, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Podaj Imię Studenta";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(239, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Podaj Nazwisko Studenta";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(374, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Podaj Wydział Studenta";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(498, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Podaj Kierunek Studenta";
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(651, 84);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 20);
+            this.Add.TabIndex = 3;
+            this.Add.Text = "Dodaj";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 172);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(244, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Podaj nr Albumu Studenta którego chcesz usunać";
+            // 
+            // nrAlbumuBox
+            // 
+            this.nrAlbumuBox.Location = new System.Drawing.Point(30, 201);
+            this.nrAlbumuBox.Name = "nrAlbumuBox";
+            this.nrAlbumuBox.Size = new System.Drawing.Size(100, 20);
+            this.nrAlbumuBox.TabIndex = 5;
+            // 
+            // DelStud
+            // 
+            this.DelStud.Location = new System.Drawing.Point(161, 201);
+            this.DelStud.Name = "DelStud";
+            this.DelStud.Size = new System.Drawing.Size(98, 23);
+            this.DelStud.TabIndex = 6;
+            this.DelStud.Text = "Usuń Studenta";
+            this.DelStud.UseVisualStyleBackColor = true;
+            this.DelStud.Click += new System.EventHandler(this.DelStud_Click);
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(293, 271);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(75, 23);
+            this.Back.TabIndex = 7;
+            this.Back.Text = "Cofnij";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // Quit
+            // 
+            this.Quit.Location = new System.Drawing.Point(374, 271);
+            this.Quit.Name = "Quit";
+            this.Quit.Size = new System.Drawing.Size(75, 23);
+            this.Quit.TabIndex = 8;
+            this.Quit.Text = "Wyjście";
+            this.Quit.UseVisualStyleBackColor = true;
+            this.Quit.Click += new System.EventHandler(this.Quit_Click);
+            // 
+            // AddDelStu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 306);
+            this.Controls.Add(this.Quit);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.DelStud);
+            this.Controls.Add(this.nrAlbumuBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.KierBox);
+            this.Controls.Add(this.WydzBox);
+            this.Controls.Add(this.NazBox);
+            this.Controls.Add(this.ImBox);
+            this.Controls.Add(this.nrAlbBox);
+            this.Controls.Add(this.label1);
+            this.Name = "AddDelStu";
+            this.Text = "Dodaj/Usuń Studenta";
+            this.Load += new System.EventHandler(this.AddDelStu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nrAlbBox;
+        private System.Windows.Forms.TextBox ImBox;
+        private System.Windows.Forms.TextBox NazBox;
+        private System.Windows.Forms.TextBox WydzBox;
+        private System.Windows.Forms.TextBox KierBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox nrAlbumuBox;
+        private System.Windows.Forms.Button DelStud;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.Button Quit;
+    }
+}
---
+++ Proj/BazaUniwersytecka/AddDelStu.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace BazaUniwersytecka
+{
+
+    public partial class AddDelStu : Form
+    {
+        static public string nrAlbumuAdd;
+        static public string nrAlbumuDel;
+        static public string Imie;
+        static public string Nazwisko;
+        static public string Wydzial;
+        static public string Kierunek;
+        public string Insert;
+        public string Delete;
+
+
+        public AddDelStu()
+        {
+            InitializeComponent();
+        }
+
+
+        //Metoda dodania Studenta
+        private void Add_Click(object sender, EventArgs e)
+        {
+            nrAlbumuAdd = nrAlbBox.Text;
+            Imie = ImBox.Text;
+            Nazwisko = NazBox.Text;
+            Wydzial = WydzBox.Text;
+            Kierunek = KierBox.Text;
+
+            LoginWindow.connect = new MySqlConnection("server = localhost; userid = root; password = admin; database=bazauniwersytecka");
+            LoginWindow.Upr = String.Format("Insert into listastudentow(nrAlbumu, Imie, Nazwisko, Wydzial, Kierunek) values('{0}','{1}','{2}','{3}','{4}')", nrAlbumuAdd, Imie, Nazwisko, Wydzial, Kierunek);
+            LoginWindow.connect.Open();
+            LoginWindow.command = new MySqlCommand(LoginWindow.Upr, LoginWindow.connect);
+            Insert = Convert.ToString(LoginWindow.command.ExecuteScalar());
+
+        }
+
+        private void AddDelStu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DelStud_Click(object sender, EventArgs e)
+        {
+            //Pobranie nr Albumu do usunięcia
+            nrAlbumuDel = nrAlbumuBox.Text;
+            LoginWindow.connect = new MySqlConnection("server = localhost; userid = root; password = admin; database=bazauniwersytecka");
+            LoginWindow.Upr = String.Format("Delete from listastudentow Where nrAlbumu = '{0}'", nrAlbumuDel);
+            LoginWindow.connect.Open();
+            LoginWindow.command = new MySqlCommand(LoginWindow.Upr, LoginWindow.connect);
+            Delete = Convert.ToString(LoginWindow.command.ExecuteScalar());
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            Admin A = new Admin();
+            this.Close();
+            this.Hide();
+
+            A.ShowDialog();
+        }
+
+        private void Quit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
