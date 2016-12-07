bf546aeda826aba66fb1d4cbe2cb4d3a7a820d5a
tc021 <sypnik21@o2.pl>
2016-12-07 14:12:45 +0100

    Możliwość wystawienia oceny studentowi

---
+++ Proj/BazaUniwersytecka/MarkForStu.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace BazaUniwersytecka
+{
+    partial class MarkForStu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nrAlbumBox = new System.Windows.Forms.TextBox();
+            this.FinishMark = new System.Windows.Forms.Label();
+            this.markBox = new System.Windows.Forms.TextBox();
+            this.SetMarkB = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(301, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numer Albumu Studenta :";
+            // 
+            // nrAlbumBox
+            // 
+            this.nrAlbumBox.Location = new System.Drawing.Point(304, 53);
+            this.nrAlbumBox.Name = "nrAlbumBox";
+            this.nrAlbumBox.Size = new System.Drawing.Size(119, 20);
+            this.nrAlbumBox.TabIndex = 1;
+            // 
+            // FinishMark
+            // 
+            this.FinishMark.AutoSize = true;
+            this.FinishMark.Location = new System.Drawing.Point(301, 100);
+            this.FinishMark.Name = "FinishMark";
+            this.FinishMark.Size = new System.Drawing.Size(93, 13);
+            this.FinishMark.TabIndex = 2;
+            this.FinishMark.Text = "Ocena Końcowa :";
+            // 
+            // markBox
+            // 
+            this.markBox.Location = new System.Drawing.Point(304, 127);
+            this.markBox.Name = "markBox";
+            this.markBox.Size = new System.Drawing.Size(119, 20);
+            this.markBox.TabIndex = 3;
+            // 
+            // SetMarkB
+            // 
+            this.SetMarkB.Location = new System.Drawing.Point(304, 169);
+            this.SetMarkB.Name = "SetMarkB";
+            this.SetMarkB.Size = new System.Drawing.Size(119, 23);
+            this.SetMarkB.TabIndex = 4;
+            this.SetMarkB.Text = "Wystaw Ocene";
+            this.SetMarkB.UseVisualStyleBackColor = true;
+            this.SetMarkB.Click += new System.EventHandler(this.SetMarkB_Click);
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(583, 271);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(75, 23);
+            this.Back.TabIndex = 5;
+            this.Back.Text = "Wróc";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // MarkForStu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 306);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.SetMarkB);
+            this.Controls.Add(this.markBox);
+            this.Controls.Add(this.FinishMark);
+            this.Controls.Add(this.nrAlbumBox);
+            this.Controls.Add(this.label1);
+            this.Name = "MarkForStu";
+            this.Text = "MarkForStu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nrAlbumBox;
+        private System.Windows.Forms.Label FinishMark;
+        private System.Windows.Forms.TextBox markBox;
+        private System.Windows.Forms.Button SetMarkB;
+        private System.Windows.Forms.Button Back;
+    }
+}
---
+++ Proj/BazaUniwersytecka/MarkForStu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace BazaUniwersytecka
+{
+    public partial class MarkForStu : Form
+    {
+        static public string nrAlbumu;
+        static public string ocenaKoncowa;
+        public string Ocena;
+        public MarkForStu()
+        {
+            InitializeComponent();
+        }
+
+
+        //przycisk powrotu
+        private void Back_Click(object sender, EventArgs e)
+        {
+            PracownikForm PFB = new PracownikForm();
+            this.Close();
+            this.Hide();
+            PFB.ShowDialog();
+        }
+
+        //Metoda dla przycisku wystawiania oceny
+        private void SetMarkB_Click(object sender, EventArgs e)
+        {
+            //Pobranie danych
+            nrAlbumu = nrAlbumBox.Text;
+            ocenaKoncowa = markBox.Text;
+
+            //Ustawienie Oceny
+            LoginWindow.connect = new MySqlConnection("server = localhost; userid = root; password = admin; database=bazauniwersytecka");
+            LoginWindow.Upr = "Update listastudentow set OcenaKoncowa ="+ocenaKoncowa+" where nrAlbumu="+nrAlbumu;
+            LoginWindow.connect.Open();
+            LoginWindow.command = new MySqlCommand(LoginWindow.Upr, LoginWindow.connect);
+            Ocena = Convert.ToString(LoginWindow.command.ExecuteScalar());
+
+        }
+    }
+}
