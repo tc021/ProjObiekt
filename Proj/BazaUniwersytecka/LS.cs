cbf5cb921ebf390fb6aaf21c7c478b7f7482d705
tc021 <sypnik21@o2.pl>
2016-12-07 09:30:51 +0100

    Dodałem wyświetlanie listy Studentów dla Studenta

--- Proj/BazaUniwersytecka/LS.cs
+++ Proj/BazaUniwersytecka/LS.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
 
 namespace BazaUniwersytecka
 {
     public partial class LS : Form
     {
+
+        
         public LS()
         {
             InitializeComponent();
@@ -20,11 +25,26 @@ namespace BazaUniwersytecka
         
         private void MenuBack_Click(object sender, EventArgs e)
         {
-            StudentForm StudWindow = new StudentForm();
+            ForStudents StudWindow = new ForStudents();
             this.Hide();
             this.Close();
 
             StudWindow.ShowDialog();
         }
+
+        private void LS_Load(object sender, EventArgs e)
+        {
+            LoginWindow.connect = new MySqlConnection("server = localhost; userid = root; password = admin; database=bazauniwersytecka");
+            LoginWindow.Upr = "Select * from listastudnetow";
+            LoginWindow.connect.Open();
+            LoginWindow.command = new MySqlCommand(LoginWindow.Upr, LoginWindow.connect);
+            DataTable dataTable = new DataTable();
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(LoginWindow.command);
+            
+            dataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable.DefaultView;
+        }
+
+       
     }
 }
