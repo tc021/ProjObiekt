94d12266ba6b865464bb62a3801347643abf28dd
tc021 <sypnik21@o2.pl>
2016-12-07 14:33:50 +0100

    Listy pracowników dla menu pracownika

---
+++ Proj/BazaUniwersytecka/WorkListForWork.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace BazaUniwersytecka
+{
+    public partial class WorkList : Form
+    {
+        public WorkList()
+        {
+            InitializeComponent();
+        }
+
+        //Wyświetlenie Listy pracowników
+        private void WorkListForWork_Load(object sender, EventArgs e)
+        {
+            LoginWindow.connect = new MySqlConnection("server = localhost; userid = root; password = admin; database=bazauniwersytecka");
+            LoginWindow.Upr = "Select * from listapracownikow";
+            LoginWindow.connect.Open();
+            LoginWindow.command = new MySqlCommand(LoginWindow.Upr, LoginWindow.connect);
+            DataTable dataTable = new DataTable();
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(LoginWindow.command);
+
+            dataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable.DefaultView;
+        }
+
+        //Przycisk powrotu
+        private void Back_Click(object sender, EventArgs e)
+        {
+            PracownikForm PF = new PracownikForm();
+            this.Close();
+            this.Hide();
+
+            PF.ShowDialog();
+
+        }
+    }
+}
