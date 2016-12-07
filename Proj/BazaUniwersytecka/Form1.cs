1496a861dd7c84caf3ed6f0d2940b96717468ca5
tc021 <sypnik21@o2.pl>
2016-12-07 07:42:37 +0100

    Add files via upload
    Dodane

---
+++ Proj/BazaUniwersytecka/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace BazaUniwersytecka
+{
+    public partial class LoginWindow : Form
+    {
+        static public string login;
+        static public string passwd;
+        public MySqlConnection connect;
+        public string Uprawnienie;
+
+
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void Logowanie_Click(object sender, EventArgs e)
+        {
+            //Pobieranie danych logowania
+            login = LoginBox.Text;
+            passwd = PassBox.Text;
+
+            //Laczenie z baza danych i sprawdzanie poprawnosci loginu i hasła
+            try
+            {
+                connect = new MySqlConnection("server=localhost;user id=" + login + "; password=" + passwd + ";database=bazauniwersytecka");
+                connect.Open();
+           
+            //Sprawdzanie uprawnien
+            MySqlCommand command;
+            string Upr = String.Format("Select Uprawnienie from uzytkownicy Where uzytkownicy.Login = '{0}'", login);
+            command = new MySqlCommand(Upr, connect);
+            Uprawnienie = Convert.ToString(command.ExecuteScalar());
+            }
+            catch (MySqlException wyjatek)
+            {
+                MessageBox.Show("Zle haslo albo login");
+            }
+
+            if (Uprawnienie == "Std")
+            {
+                PracownikForm st = new PracownikForm();
+                this.Hide(); 
+                st.ShowDialog();
+            }
+            
+            else if(Uprawnienie == "Pr")
+            {
+                PracownikForm pr = new PracownikForm();
+                this.Hide();
+                pr.ShowDialog();
+            }
+            else if(Uprawnienie == "Admin")
+            {
+                Admin Ad = new Admin();
+                this.Hide();
+                Ad.ShowDialog();
+            }
+            
+        }
+
+        //Metoda dla przycisku "Wyjście"
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void LoginWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Proj/BazaUniwersytecka/LS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BazaUniwersytecka
+{
+    public partial class LS : Form
+    {
+        public LS()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void MenuBack_Click(object sender, EventArgs e)
+        {
+            StudentForm StudWindow = new StudentForm();
+            this.Hide();
+            this.Close();
+
+            StudWindow.ShowDialog();
+        }
+    }
+}
