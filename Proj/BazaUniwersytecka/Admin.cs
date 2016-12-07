27d706f762ada97d449c2e1ef83bbc29e8b88cd5
tc021 <sypnik21@o2.pl>
2016-12-07 17:01:50 +0100

    Aktualizacja admina

--- Proj/BazaUniwersytecka/Admin.cs
+++ Proj/BazaUniwersytecka/Admin.cs
@@ -17,9 +17,56 @@ namespace BazaUniwersytecka
             InitializeComponent();
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        //Przejcie do Listy stodentow dla admina
+        private void StudentList_Click(object sender, EventArgs e)
         {
+            ListSAdmin LSA = new ListSAdmin();
+            this.Close();
+            this.Hide();
 
+            LSA.ShowDialog();
+
+        }
+
+        //Przejscie do Wyszukiwania Studentów dla admina
+        private void StudSearch_Click(object sender, EventArgs e)
+        {
+            StSearchA SSA = new StSearchA();
+            this.Close();
+            this.Hide();
+
+            SSA.ShowDialog();
+
+        }
+
+        //Przycisk do przejscia do okna dodawania/usuwania studenta
+        private void AddDelStud_Click(object sender, EventArgs e)
+        {
+            AddDelStu ADS = new AddDelStu();
+            this.Close();
+            this.Hide();
+
+            ADS.ShowDialog();
+        }
+
+        //Modyfikowanie Studenta
+        private void ModifyStudent_Click(object sender, EventArgs e)
+        {
+            ModStudent MS = new ModStudent();
+            this.Close();
+            this.Hide();
+
+            MS.ShowDialog();
+        }
+
+        //Modyfikowanie tabeli studenci
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ModTabStu MTS = new ModTabStu();
+            this.Close();
+            this.Hide();
+
+            MTS.ShowDialog();
         }
     }
 }
