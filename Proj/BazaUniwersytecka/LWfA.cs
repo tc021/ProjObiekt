e5ba5b64c30e37ac5b498c1d2cd1badba570a949
tc021 <sypnik21@o2.pl>
2016-12-08 15:44:28 +0100

    TabelaPracowników dla admina

---
+++ Proj/BazaUniwersytecka/LWfA.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace BazaUniwersytecka
+{
+    public partial class LWfA : Form
+    {
+        public LWfA()
+        {
+            InitializeComponent();
+        }
+
+        private void LWfA_Load(object sender, EventArgs e)
+        {
+            LoginWindow.connect = new MySqlConnection("server = localhost; userid = root; password = admin; database=bazauniwersytecka");
+            LoginWindow.Upr = "Select * from listapracownikow";
+            LoginWindow.connect.Open();
+            LoginWindow.command = new MySqlCommand(LoginWindow.Upr, LoginWindow.connect);
+            DataTable dataTable = new DataTable();
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(LoginWindow.command);
+
+            dataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable.DefaultView;
+        }
+
+
+        //Przycisk cofniecia
+        private void Back_Click(object sender, EventArgs e)
+        {
+            Admin a = new BazaUniwersytecka.Admin();
+            this.Close();
+            this.Hide();
+
+            a.ShowDialog();
+        }
+    }
+}
